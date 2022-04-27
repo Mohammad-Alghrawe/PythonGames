0bf0545d4df17f127cbc42767dd06a552ff99f5a
Mohammad Alghrawe <mohammad.alghrawe@gmail.com>
2022-04-27 12:19:02 +0200

    created gameboard with pc function , initialis the game board, and play function

--- TecTacToe/tectactoe.py
+++ TecTacToe/tectactoe.py
@@ -166,6 +166,90 @@ def get_text_pc(i, j, gb, l1, l2):
             get_text_pc(move[0], move[1], gb, l1, l2)
 
 # 10- create the GUI of gameboard for play along with pc.
+
+
+def gameboard_pc(game_board, l1, l2):
+    global button
+    button = []
+    # range(0,3) => 0,1,2
+    for i in range(3):
+        m = 3+i
+        # append i to 3 x 3 of btns
+        button.append(i)
+        button[i] = []
+        for j in range(3):
+            n = j
+            button[i].append(j)
+            get_t = partial(get_text_pc, i, j, game_board, l1, l2)
+            button[i][j] = Button(
+                game_board, bd=5, command=get_t, height=4, width=8)
+            ''' 
+            The Grid geometry manager puts the widgets in a 2-dimensional table. The master widget is split into a number of rows and columns, and each “cell” in the resulting table can hold a widget.
+            '''
+            button[i][j].grid(row=m, column=n)
+    game_board.mainloop()
 # 11- initialize the game board to play with pc.
+
+
+def withpc(game_board):
+    # destroy the widget. quit() stops the mainloop
+    game_board.destroy()
+    game_board = Tk()
+    game_board.title("Tic Tac Toe")
+    l1 = Button(game_board, text="Player : X", width=10)
+    l1.grid(row=1, column=1)
+    l2 = Button(game_board, text="Computer : O",
+                width=10, state=DISABLED)
+
+    l2.grid(row=2, column=1)
+    gameboard_pc(game_board, l1, l2)
 # 12- initialize the game board to play with another.
+
+
+def withplayer(game_board):
+    game_board.destroy()
+    game_board = Tk()
+    game_board.title("Tic Tac Toe")
+    l1 = Button(game_board, text="Player 1 : X", width=10)
+
+    l1.grid(row=1, column=1)
+    l2 = Button(game_board, text="Player 2 : O",
+                width=10, state=DISABLED)
+
+    l2.grid(row=2, column=1)
+    gameboard_pl(game_board, l1, l2)
 # 13- main function to run all the prevs functions and create the gui root.
+def play():
+    menu = Tk()
+    menu.geometry("250x250")
+    menu.title("Tic Tac Toe")
+    wpc = partial(withpc, menu)
+    wpl = partial(withplayer, menu)
+      
+    head = Button(menu, text = "---Welcome to tic-tac-toe---",
+                  activeforeground = 'red',
+                  activebackground = "yellow", bg = "red", 
+                  fg = "yellow", width = 500, font = 'summer', bd = 5)
+      
+    B1 = Button(menu, text = "Single Player", command = wpc, 
+                activeforeground = 'red',
+                activebackground = "yellow", bg = "red", 
+                fg = "yellow", width = 500, font = 'summer', bd = 5)
+      
+    B2 = Button(menu, text = "Multi Player", command = wpl, activeforeground = 'red',
+                activebackground = "yellow", bg = "red", fg = "yellow",
+                width = 500, font = 'summer', bd = 5)
+      
+    B3 = Button(menu, text = "Exit", command = menu.quit, activeforeground = 'red',
+                activebackground = "yellow", bg = "red", fg = "yellow",
+                width = 500, font = 'summer', bd = 5)
+    head.pack(side = 'top')
+    B1.pack(side = 'top')
+    B2.pack(side = 'top')
+    B3.pack(side = 'top')
+    menu.mainloop()
+
+
+# Call main function
+if __name__ == '__main__':
+    play()
