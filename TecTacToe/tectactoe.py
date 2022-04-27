71481718ac365645f75ea21a3b2e94ede2036821
Mohammad Alghrawe <mohammad.alghrawe@gmail.com>
2022-04-27 11:07:35 +0200

    added todo list of the game to start coding

--- TecTacToe/tectactoe.py
+++ TecTacToe/tectactoe.py
@@ -9,3 +9,18 @@ import random
 from functools import partial
 # This module provides generic shallow and deep copy operations.
 from copy import deepcopy
+
+# Todo list:
+# 1- create an empty board.
+# 2- create a variable to decide  the turn of each player.
+# 3- function to check X/O won the match or not according to the rules.
+# 4- configure the text on btn in tow player mode.
+# 5- function to check if the place empty or not.
+# 6- function to check the whole board.
+# 7- create gameboard GUI for tow player mode.
+# 8- function for deciding the moves of the system.
+# 9- configure text on btn in one player mode.
+# 10- create the GUI of gameboard for play along with pc.
+# 11- initialize the game board to play with pc.
+# 12- initialize the game board to play with another.
+# 13- main function to run all the prevs functions and create the gui root.
