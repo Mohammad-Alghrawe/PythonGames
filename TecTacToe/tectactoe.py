efae38606dcba0b899cd8e5ef87d34a2ef3572f2
Mohammad Alghrawe <mohammad.alghrawe@gmail.com>
2022-04-27 10:47:06 +0200

    imported the necessary libraries for the games

---
+++ TecTacToe/tectactoe.py
@@ -0,0 +1,11 @@
+# Mohammad Alghrawe
+# TecTacTeo with GUI using Tkinter
+# importing all the necessary libraries.
+# Tkinter GUI library.
+from tkinter import *
+# This module implements pseudo-random number generators.
+import random
+# module is for higher-order functions: functions that act on or return other functions.
+from functools import partial
+# This module provides generic shallow and deep copy operations.
+from copy import deepcopy
